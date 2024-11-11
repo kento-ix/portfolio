907c05c3814c20e5b636f953b3f3def9d456ca5d
kento-ix <kentoix1105@gmail.com>
2024-11-11 00:41:56 -0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
+
+    fetch("components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-placeholder").innerHTML = data;
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hungerMenu = document.getElementById('hunger-menu'); // ハンバーガーメニュー
+    const menu = document.getElementById('menu'); // メニュー
+  
+    // ハンバーガーメニューがクリックされた時の処理
+    hungerMenu.addEventListener('click', function () {
+      // モバイルメニューの表示・非表示を切り替え
+      menu.classList.toggle('show');
+    });
+  });
+  

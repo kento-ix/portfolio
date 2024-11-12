f331aa282aa686917dd4611cc4987f507beab8e2
kento-ix <kentoix1105@gmail.com>
2024-11-11 18:01:46 -0800

    added about pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("components/header.html")
+    fetch("../components/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;

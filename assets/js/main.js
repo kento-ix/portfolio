b0119b5632076cf9b051b4ee9d3d7e890b7f7011
kento-ix <kentoix1105@gmail.com>
2024-11-12 22:54:40 -0800

    add personal work card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // headerの読み込み
     fetch("../components/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;
         });
 
-    fetch("components/footer.html")
+    // footerの読み込み
+    fetch("../components/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-placeholder").innerHTML = data;
         });
 });
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const hungerMenu = document.getElementById('hunger-menu'); // ハンバーガーメニュー
@@ -21,5 +25,4 @@ document.addEventListener("DOMContentLoaded", function () {
       // モバイルメニューの表示・非表示を切り替え
       menu.classList.toggle('show');
     });
-  });
-  
+});

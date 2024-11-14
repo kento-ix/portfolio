9b12282913a813fc47d19753cd8e07643cc68680
kento-ix <kentoix1105@gmail.com>
2024-11-13 17:59:00 -0800

    update skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,27 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const starsContainer = document.querySelectorAll('.stars');
+    
+    starsContainer.forEach(function(stars) {
+        const rating = parseInt(stars.getAttribute('data-rating'));
+        let starsHtml = '';
+
+        //generate star based on given num
+        for (let i = 0; i < 5; i++) {
+            if (i < rating) {
+                starsHtml += '<i class="fa fa-star filled"></i>';
+            } else {
+                starsHtml += '<i class="fa fa-star"></i>';
+            }
+        }
+
+        stars.innerHTML = starsHtml;
+    });
+});
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
